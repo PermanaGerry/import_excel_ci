71e71c442ee184282edf6d934496afdfeff324c1
PermanaGerry <permana.gerry@gmail.com>
2018-10-02 13:26:41 +0700

    pengecekan data di excel double

--- application/controllers/Excell.php
+++ application/controllers/Excell.php
@@ -43,49 +43,10 @@ class Excell extends CI_Controller {
         //send the data in an array format
         $data['header'] = $header;
         $data['values'] = $arr_data;
-
-        $out = array();
-        $unknown_data = array();
-        $double['data_duplicate']['B'] = array();
-
-        foreach ($data['values'] as $key => $value)
-        {
-            foreach ($data['values'] as $key2 => $value2)
-            {
-                if (!array_key_exists('B', $value) && !array_key_exists('D', $value) && !array_key_exists('F', $value))
-                {
-                    $unknown_data['unknown_data'][$key2] = $value;
-                }
-
-                if ($key != $key2)
-                {
-                    if (($value['B'] == $value2['B']))
-                    {
-                        $double['data_duplicate']['B'][] = $key2;
-                    }
-
-                    if (($value['D'] == $value2['D']))
-                    {
-                        $double['data_duplicate']['D'][] = $key2;
-                    }
-
-                    if (($value['F'] == $value2['F']))
-                    {
-                        $double['data_duplicate']['F'][] = $key2;
-                    }
-                }
-            }
-        }
         
-        $data_double = array_keys(
-                array_count_values(
-                        array_merge($double['data_duplicate']['B'], $double['data_duplicate']['D'], $double['data_duplicate']['F'])
-                        )
-                );
-        
-        print_r($data_double);
+        $data_double = $this->_duplicate_data($data);
         // save data
-        //$this->_save($data['values']);
+        $this->_save($data['values'], $data_double);
     }
 
     public function writer()
@@ -118,9 +79,63 @@ class Excell extends CI_Controller {
         $objWriter->save('php://output');
     }
 
-    public function _save($data = '')
+    public function _save($save = '', $duplicate = '')
     {
+        foreach ($duplicate as $key => $value)
+        {
+            unset($save[$value]);
+        }
+        
+        // input data to database
         $this->kelas_model->save($data);
     }
 
+    public function _duplicate_data($data = '')
+    {
+        if (!is_array($data))
+        {
+            return FALSE;
+        }
+
+        // search data duplicate
+        $out = array();
+        $unknown_data = array();
+        $double['data_duplicate']['B'] = array();
+        $double['data_duplicate']['D'] = array();
+        $double['data_duplicate']['F'] = array();
+        foreach ($data['values'] as $key => $value)
+        {
+            foreach ($data['values'] as $key2 => $value2)
+            {
+                if (!array_key_exists('B', $value) && !array_key_exists('D', $value) && !array_key_exists('F', $value))
+                {
+                    $unknown_data['unknown_data'][$key2] = $value;
+                }
+
+                if ($key != $key2)
+                {
+                    if (($value['B'] == $value2['B']))
+                    {
+                        $double['data_duplicate']['B'][] = $key2;
+                    }
+
+                    if (($value['D'] == $value2['D']))
+                    {
+                        $double['data_duplicate']['D'][] = $key2;
+                    }
+
+                    if (($value['F'] == $value2['F']))
+                    {
+                        $double['data_duplicate']['F'][] = $key2;
+                    }
+                }
+            }
+        }
+
+        return array_keys(
+                array_count_values(
+                        array_merge($double['data_duplicate']['B'], $double['data_duplicate']['D'], $double['data_duplicate']['F'])
+                )
+        );
+    }
 }
